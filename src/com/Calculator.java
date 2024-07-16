603da5ce4553f5adbde5e9d0f40fcf0a0017c3f6
Your Name <saqlainzakaria05@gmail.com>
2024-07-16 16:02:22 +0530

    Added: Subtraction functionality added

--- src/com/Calculator.java
+++ src/com/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
         int a = 10, b=20, c;
         c = a + b;
         System.out.println("Addition is c" + c);
-
+        c = a - b;
+        System.out.println("Subtraction is c" + c);
     }
 }
